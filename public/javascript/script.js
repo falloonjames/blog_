b3edc69ec291676de84dfd4f2b85e98a990543d3
James F <falloonjames@gmail.com>
2020-02-07 22:46:28 -0500

    add create function to redirect

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,8 @@
-// $('#cancel-button')
 function gohome(){
     window.location.href = "/";
-}
+    document.querySelector('#par').innerText = users[0];
+};
+
+function create(){
+    window.location.href = "/blogs/new";
+};
